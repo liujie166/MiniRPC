10e3aba37415688537ee4cf7d1caa59353f420ff
liujie <906756461@qq.com>
2021-04-16 23:09:00 +0800

    meaningless commit

--- rdma_common.h
+++ rdma_common.h
@@ -1,6 +1,26 @@
-#define _RDMA_COMMON_H_
-#ifndef _RDMA_COMMON_H_
+#define RDMA_COMMON_H_
+#ifndef RDMA_COMMON_H_
+
+#include <stdio.h>
+#include <stdbool.h>
+
+#include <infiniband/verbs.h>
+/* rdma control path */
+struct RdmaResource
+{
+
+};
+/**
+* RdmaInit - resolve rdma device port and create needed resource
+* @param DeviceName   : RDMA device name, default the first active one
+* @param Memory       : pre-allocated memory
+* @param MemorySize   :
+* @return : true on success, false on error
+**/
+bool RdmaInit(char* DeviceName, void* Memory, size_t MemorySize);
+
+bool RdmaCreateQueuePair();
 
 
-#endif // !_RDMA_COMMON_H_
+#endif // !RDMA_COMMON_H_
 
